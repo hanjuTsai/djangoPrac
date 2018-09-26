bf6577ff054cce2cd5be0fdef4923acb1a50b113
HanjuTsai <rita@dentall.io>
2018-09-26 11:42:02 +0800

    add the static images file

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+
+
+# URL
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Create an template directory
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_URL = '/static/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -105,6 +109,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
@@ -120,8 +131,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 django_heroku.settings(locals())
--- helloworld/urls.py
+++ helloworld/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index)
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
