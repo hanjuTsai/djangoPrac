1e7192da62c6495414f1a7f8df25913916062684
HanjuTsai <rita@dentall.io>
2018-09-27 22:38:06 +0800

    add local file

--- helloworld/views.py
+++ helloworld/views.py
@@ -7,7 +7,8 @@ import os
 from helloworld.settings import STATICFILES_DIRS
 
 def index(request):
-    p = "/Users/hanjitsai/Documents/csx/code/djangoPrac/static/images"  # insert the path to your directory
+    #p = "/Users/hanjitsai/Documents/csx/code/djangoPrac/static/images"  # insert the path to your directory
+    p = os.path.join(STATICFILES_DIRS[0],'images')
     img_list = os.listdir(p)
     return render(request, 'board.html', {'images': img_list})
 
