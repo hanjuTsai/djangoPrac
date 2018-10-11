678ca5142100bd68eb6cf288ad30fa61a33197e7
HanjuTsai <rita@dentall.io>
2018-10-12 00:46:19 +0800

    set up the database

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -85,12 +85,25 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#      }
+# }
+
 DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hanjuprac',
+        'USER': 'admin',
+        'PASSWORD' : 'admin',
+        'HOST' : '',
+        'PORT' : '',
+    }
 }
+
+
 #import dj_database_url
 # DATABASES = {
 #     'default': dj_database_url.config(),
